4b7aa584c95889db1c609475f3fc7f3826cc80e2
Will Nzeuton <wnzeuton50@stuy.edu>
2024-09-27 11:46:13 -0400

    more extracurricular

--- 12_flask-jinja/app.py
+++ 12_flask-jinja/app.py
@@ -31,7 +31,8 @@ def hello_world():
     return "No hablo queso!"
 coll = [0,1,1,2,3,5,8]
 chant = [["WE ARE STRONG","WE BELONG","HEAR OUR SONG", "WE MARCH ALONG"], ["WE WONT FALL", "WE GIVE OUR ALL", "HEED OUR CALL", "WE WONT STALL"]]
-gallery = ["topher.jpeg", "topher2.png"]
+gallery = ["topher.jpeg", "topher2.png", "topher3.png"]
+res = ["https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.stuycs.org/faculty/&ved=2ahUKEwjgmYH0ueOIAxX8FVkFHUYmJLUQFnoECBQQAQ&usg=AOvVaw0HkCpt-9OhSNnE2UfQcw6A","https://www.google.com/url?sa=i&url=https%3A%2F%2Fcestlaz.github.io%2Fposts%2F2013-10-31-halloween%2F&psig=AOvVaw1DS6i-3DPNCCN8NVU7AA4Y&ust=1727536752383000&source=images&cd=vfe&opi=89978449&ved=0CBcQjhxqFwoTCLCVkYa244gDFQAAAAAdAAAAABAE"]   
 art = []
 with open('static/art.txt', 'r') as art:
     lines = [x.replace('\n', '') for x in art.readlines()]
@@ -48,7 +49,7 @@ def test_tmplt():
     return render_template( 'model_tmplt.html', foo="fooooo", collection=coll)
 @app.route("/topher")
 def toph():
-    return render_template('topher_tmplt.html', foo = "Home", collection=chant, images = gallery, art = art)
+    return render_template('topher_tmplt.html', foo = "TOPHER LOVERS HOME", collection=chant, images = gallery, art = art, resources = res)
 
 if __name__ == "__main__":
     app.debug = True
