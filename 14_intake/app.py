7be152eb5230dae56db7fc707cba8ba1a9078131
Will Nzeuton <wnzeuton50@stuy.edu>
2024-10-09 10:49:23 -0400

    Changed methods to POST

--- 14_intake/app.py
+++ 14_intake/app.py
@@ -41,34 +41,34 @@ PROTIP: Insert your own in-line comments
 
 @app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    print("***DIAG: request.args['username']  ***")
-    print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
+#     print("\n\n\n")
+#     print("***DIAG: this Flask obj ***")
+#     print(app)
+#     print("***DIAG: request obj ***")
+#     print(request)
+#     print("***DIAG: request.args ***")
+#     print(request.args)
+#     print("***DIAG: request.args['username']  ***")
+#     print(request.args['username'])
+#     print("***DIAG: request.headers ***")
+#     print(request.headers)
     return render_template( 'login.html' )
 
 
-@app.route("/auth", methods=['GET', 'POST'])
+@app.route("/auth", methods=['POST'])
 def authenticate():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    print("***DIAG: request.args['username']  ***")
-    print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    return request.args['username']  #response to a form submission
+#     print("\n\n\n")
+#     print("***DIAG: this Flask obj ***")
+#     print(app)
+#     print("***DIAG: request obj ***")
+#     print(request)
+#     print("***DIAG: request.args ***")
+#     print(request.args)
+#     print("***DIAG: request.args['username']  ***")
+#     print(request.args['username'])
+#     print("***DIAG: request.headers ***")
+#     print(request.headers)
+    return request.form['username']  #response to a form submission
 
 
     
