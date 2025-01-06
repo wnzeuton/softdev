529c6db471baac449e3500936aaaeae69412d661
Will Nzeuton <wnzeuton50@stuy.edu>
2025-01-06 11:47:23 -0500

    reinit 0.5

---
+++ 27_js/fibfac.js
@@ -0,0 +1,26 @@
+//Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon
+//SoftDev pd0
+//K27 - Basic functions in JavaScript
+//2025-01-06m
+
+//JavaScript implementations of Day0 recursive Scheme functions
+
+//factorial:
+
+//<your team's fact(n) implementation>
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+
+//-----------------------------------------------------------------
+
+
+//fib:
+
+//<your team's fib(n) implementation>
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+//=================================================================
