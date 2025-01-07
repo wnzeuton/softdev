77aaef538fe2060acdc5b44d63cb6aced445118e
Will Nzeuton <wnzeuton50@stuy.edu>
2025-01-06 22:26:33 -0500

    js

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,26 +1,46 @@
-//Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon
-//SoftDev pd0
+//Team bo :: Will Nzeuton, Tim Ng
+//SoftDev pd5
 //K27 - Basic functions in JavaScript
 //2025-01-06m
+
 
 //JavaScript implementations of Day0 recursive Scheme functions
 
 //factorial:
-
-//<your team's fact(n) implementation>
-
+let fact = function(n){
+    if(n == 0){
+        return 1;
+    } 
+    return n * fact(n - 1)
+}
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
-
-
+fact(1) == 1
+fact(2) == 2
+fact(3) == 6
+fact(4) == 24
+fact(5) == 120
+fact(6) == 720
+fact(7) == 5040
+fact(10) == 3628800
 //-----------------------------------------------------------------
 
 
 //fib:
-
-//<your team's fib(n) implementation>
-
+let fib = function(n){
+    if(n < 2){
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2)
+}
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
-
+fib(0) == 0
+fib(1) == 1
+fib(2) == 1
+fib(3) == 2
+fib(4) == 3
+fib(5) == 5
+fib(6) == 8
+fib(10) == 55
 //=================================================================
